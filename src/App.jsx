a987b125df5a9fed2346008d461703f82c4562d6
Zach Wagner <zach.wagner@spothero.com>
2018-10-08 20:40:43 -0500

    Add project to copy when cli creates new project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App = props => {
+    return (
+        <div className="App">
+            React + Parcel
+        </div>
+    );
+};
+
+export default App;
